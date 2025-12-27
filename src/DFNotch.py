a1eab8c851c02642e7d2d947780eca1878da23ea
AhmedsElysium <ahmadyassermo@gmail.com>
2025-12-28 01:57:06 +0200

    added notchDF filter designer

--- src/DFNotch.py
+++ src/DFNotch.py
@@ -1,7 +1,16 @@
+import numpy as np
+from IIR import _pz2iir
 """
 Implement the Direct-Form Notch Filter
 """
 
-def NotchDF(fc,fs,B):
+def _NotchDF(fc,fs,B):
+
+    z=np.exp(fc/fs*2*np.pi*1j)
+
+    b,a=_pz2iir([zz*(1-B/fs),np.conj(zz)*(1-B/fs)],
+            [zz,np.conj(zz)])
+    return b,a
+
     pass
 
